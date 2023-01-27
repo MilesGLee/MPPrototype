60a518cc4884d579fbcbb182d7cb550fbfd1698c
Miles <miles.gr.lee@gmail.com>
2023-01-27 14:22:58 -0600

    Initial Unreal Commit

---
+++ Source/MPPrototype/MPPrototypeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MPPrototypeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MPPROTOTYPE_API AMPPrototypeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
